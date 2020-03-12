dde603c4023fef92f375a1b60c78d32568f77ba2
Florent Vandroy <florentvandroy@gmail.com>
2020-03-12 23:42:36 +0100

    Laisser uniquement TLS

--- main/settings.py
+++ main/settings.py
@@ -146,7 +146,6 @@ EMAIL_HOST= config('EMAIL_HOST')
 EMAIL_PORT = config('EMAIL_PORT', cast=int)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD =config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_SSL= config('EMAIL_USE_SSL')
 EMAIL_USE_TLS= config('EMAIL_USE_TLS')
 
 # Email à laquelle envoyer les formulaires de contact
